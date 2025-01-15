74bdbd640bb4e609a507bbeea7f25e771652543f
Arthur Mustafin <mustafinrtz.2000@gmail.com>
2025-01-15 19:02:21 +0300

    [dev] Add Review and List of Reviews componetns
    A map was also connected for the Offer page with 3
    nearest markers. Added a component for upcoming offers.
    The logic of all offer lists and cards has been redesigned for improved reuse.

---
+++ src/types/review.ts
@@ -0,0 +1,12 @@
+export type Review = {
+    id: number;
+    rating: 1 | 2 | 3 | 4 | 5;
+    text: string;
+    user: {
+      name: string;
+      avatarUrl: string;
+      isPro: boolean;
+    };
+    date: string;
+  };
+  
